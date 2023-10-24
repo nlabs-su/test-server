4acc4fd10b8c6e1a4205d676f0e14b1f3de38cf7
dkubatin <kubatin_denis@mail.ru>
2023-10-24 21:48:28 +0300

    fix doc init_base_data

--- cli.py
+++ cli.py
@@ -16,7 +16,7 @@ def cli():
 @cli.command('base-data')
 def init_base_data():
     """
-    Обработчик команды `quart init base-data` - инициализация добавления базовых данных.
+    Обработчик команды `base-data` - инициализация добавления базовых данных.
     """
     success = asyncio.get_event_loop().run_until_complete(base_data.init_base_data())
     click.echo('Базовые данные - успешно добавлены!' if success else 'Базовые данные - уже существуют!')
